8680076a0005166dba496790a0a106b31148d775
Isara Nakavisute <Isara Nakavisute@DESKTOP-B3FTLE4>
2022-12-18 01:28:16 +0700

    TEST

--- banksystem/Properties/Resources.Designer.cs
+++ banksystem/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace brandnewday.Properties {
+namespace banksystem.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace brandnewday.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("brandnewday.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("banksystem.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
